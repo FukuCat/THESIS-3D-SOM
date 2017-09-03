e57c72c4ca5c02a54403cded6d001520983b7a93
Kenji Fukuoka <kenji_fukuoka@dlsu.edu.ph>
2017-09-04 07:26:16 +0800

    made endpoints of lines intersect

--- Somphony3D/core/src/com/dlsu/somphony/model/SomphonyLine.java
+++ Somphony3D/core/src/com/dlsu/somphony/model/SomphonyLine.java
@@ -30,12 +30,13 @@ public class SomphonyLine extends GameObject {
 
         points = new Array<Vector3>();
         points.add(new Vector3(0.0f, 0.0f, 0.0f));
-        for(int i = 1; i <= 10; i++){
+        for(int i = 1; i < 10; i++){
             points.add(new Vector3(
                     MathUtils.random(-0.2f, 0.2f) + points.get(i - 1).x,
                     MathUtils.random(-0.2f, 0.2f) + points.get(i - 1).y,
                     1.0f * i));
         }
+        points.add(new Vector3(0.0f, 0.0f, 10.0f));
         System.out.println(points.size);
         for(int i = 0; i < points.size; i++){
             System.out.println(points.get(i).x+":"+points.get(i).y+":"+points.get(i).z);
